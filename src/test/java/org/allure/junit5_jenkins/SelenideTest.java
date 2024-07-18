1e473e5f25c1aab840964504dd7a83d1b8c2e3ce
rattus-aristarchus <lankin.m.a@gmail.com>
2024-07-18 10:38:53 +0300

    add_selenoid

--- src/test/java/org/allure/junit5_jenkins/SelenideTest.java
+++ src/test/java/org/allure/junit5_jenkins/SelenideTest.java
@@ -1,6 +1,7 @@
 package org.allure.junit5_jenkins;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.Allure;
 import io.qameta.allure.selenide.AllureSelenide;
@@ -16,6 +17,11 @@ public class SelenideTest {
     @BeforeAll
     static void setupAllureReports() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
+    @BeforeAll
+    static void setupSelenoid() {
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 
     @Test
